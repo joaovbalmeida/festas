b77da76fb35dba60ce38474a368b76007099572b
joaovbalmeida <joaovbalmeida@gmail.com>
2019-05-24 02:53:36 -0300

    1.0

--- template/models.py
+++ template/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField("Nome", max_length=255)
+    month = models.CharField("Mês", max_length=255)
+    day = models.CharField("Dia", max_length=255)
+    link = models.CharField("Link", max_length=255)
+    code = models.CharField("Código", max_length=255)
+    short_description = models.TextField("Descrição Curta", null=True, blank=True)
+    description = models.TextField("Descrição", null=True, blank=True)
+    image = models.ImageField("Imagem", upload_to='eventimage/%Y/%m/%d/', max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField("Criado em", auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Evento'
+        verbose_name_plural = 'Eventos'
+
+    def __str__(self):
+        return self.name
--- template/views.py
+++ template/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from .models import Event
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -6,11 +7,13 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        context['events'] = Event.objects.all()
         return context
 
 class EventsView(TemplateView):
     template_name = "event.html"
-        
+
     def get_context_data(self, **kwargs):
         context = super(EventsView, self).get_context_data(**kwargs)
+        context['event'] = Event.objects.get(pk=kwargs['event_id'])
         return context
